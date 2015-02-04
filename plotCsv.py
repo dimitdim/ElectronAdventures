5cf53a5bfc29b6cf86f340a843c60ae3814e012d
Dimitar Dimitrov <dimitar.dimitrov@students.olin.edu>
2015-02-03 21:17:13 -0500

    eh

--- plotCsv.py
+++ plotCsv.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import numpy as np
 path=raw_input('Filename: ')
 f=open(path,'r')
 x=[]
@@ -8,7 +9,7 @@ for line in f:
     raw=line.split(',')
     x.append(float(raw[0]))
     y.append(float(raw[1]))
-p=plt.polyfit(x,y,1)
+p=np.polyfit(x,y,1)
 plt.scatter(x,y)
-plt.scatter(x,p*x,'-')
+#plt.scatter(x,p*x,'-')
 plt.show()
