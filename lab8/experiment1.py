8bf59b600e0295124f5ec72ba5f6c847c00f77f4
Thomas Nattestad <thomas.nattestad@gmail.com>
2015-04-15 22:41:26 -0400

    collected exp1 data

---
+++ lab8/experiment1.py
@@ -0,0 +1,45 @@
+import smu
+
+def linspace(initial, final, n = 100):
+    if n>=2:
+        increment = (float(final) - float(initial))/(n - 1)
+        return [float(initial) + i*increment for i in range(n)]
+    else:
+        return []
+
+s = smu.smu()
+
+# v2 = 2.5
+v1 = linspace(0, 5.0, 501)
+f = open('experiment1DataStrongV2=4v5.csv', 'w')
+f.write('"V1","Vout1"\n')
+
+measurements = [[],[],[]]
+
+s.set_current(2, 0.)
+s.set_autorange(1, 1)
+s.set_autorange(2, 1)
+
+for val in v1:
+    s.set_voltage(1, val)
+    measurements[0] += [s.get_voltage(2)]
+
+for val in v1:
+    f.write('{!s},{!s}\n'.format(val, measurements[0].pop(0)))
+
+s.set_voltage(1, 0.)
+s.set_voltage(2, 0.)
+f.close()
+
+
+#Vb = .5v
+#V2 1 = 2.5v
+#V2 2 = 3.5v
+#V2 3 = 4.5v
+
+#Strong inversion
+#Vb = 1v
+#V2 1 = 2.5v
+#V2 2 = 3.5v
+#V2 3 = 4.5v
+
---
+++ lab8/experiment2.py
@@ -0,0 +1,30 @@
+import smu
+
+def linspace(initial, final, n = 100):
+    if n>=2:
+        increment = (float(final) - float(initial))/(n - 1)
+        return [float(initial) + i*increment for i in range(n)]
+    else:
+        return []
+
+s = smu.smu()
+
+v2 = 2.5
+v1 = linspace(v2-.2, v2+.2, 501)
+f = open('experiment2DataA.csv', 'w')
+f.write('"V1","Vout"\n')
+
+s.set_current(2, 0.)
+for val in v1:
+    s.set_voltage(1, val)
+    s.autorange(1)
+    s.autorange(2)
+    f.write('{!s},{!s}\n'.format(val, -1*measurements[0].pop(0)))
+    measurements[0] += [s.get_voltage(2)]
+
+for val in v1:
+    
+
+s.set_voltage(1, 0.)
+s.set_voltage(2, 0.)
+f.close()
